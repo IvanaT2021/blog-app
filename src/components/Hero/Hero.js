762a61ba68adea4b6e6b5fbdec929f2e51653ea4
unknown <aliceinkland@gmail.com>
2021-11-25 20:58:35 +0100

    small fix

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,16 +1,16 @@
 import React from "react";
 import Navbar from "./NavBar/NavBar";
-import './Hero.modules.css'
-import  ButtonField  from "./Button/Button";
+import "./Hero.modules.css";
+import ButtonField from "./Button/ButtonField";
 
-const Hero =()=>{
-    return(
-        <div className="hero">
-            <h1>Hello from Hero</h1>
-            <Navbar/>
-            <ButtonField/>
-        </div>
-    )
-}
+const Hero = () => {
+  return (
+    <div className="hero">
+      <h1>Hello from Hero</h1>
+      <Navbar />
+      <ButtonField />
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
